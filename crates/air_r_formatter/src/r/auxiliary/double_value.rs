469d027d6a272ff0bd64bfb69efcb835a543a7fd
Davis Vaughan <davis@posit.co>
2024-10-30 13:38:08 -0400

    Add double tests

--- crates/air_r_formatter/src/r/auxiliary/double_value.rs
+++ crates/air_r_formatter/src/r/auxiliary/double_value.rs
@@ -1,10 +1,13 @@
 use crate::prelude::*;
 use air_r_syntax::RDoubleValue;
-use biome_formatter::token::number::format_number_token;
+use air_r_syntax::RDoubleValueFields;
+use biome_formatter::write;
+
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatRDoubleValue;
 impl FormatNodeRule<RDoubleValue> for FormatRDoubleValue {
     fn fmt_fields(&self, node: &RDoubleValue, f: &mut RFormatter) -> FormatResult<()> {
-        format_number_token(&node.value_token()?).fmt(f)
+        let RDoubleValueFields { value_token } = node.as_fields();
+        write![f, [value_token.format()]]
     }
 }
