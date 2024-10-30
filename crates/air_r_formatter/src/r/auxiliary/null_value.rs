d2eaede9ed0142a11239846b253683c50034327b
Davis Vaughan <davis@posit.co>
2024-10-30 13:52:23 -0400

    Add `NULL` test

--- crates/air_r_formatter/src/r/auxiliary/null_value.rs
+++ crates/air_r_formatter/src/r/auxiliary/null_value.rs
@@ -1,10 +1,13 @@
 use crate::prelude::*;
 use air_r_syntax::RNullValue;
-use biome_rowan::AstNode;
+use air_r_syntax::RNullValueFields;
+use biome_formatter::write;
+
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatRNullValue;
 impl FormatNodeRule<RNullValue> for FormatRNullValue {
     fn fmt_fields(&self, node: &RNullValue, f: &mut RFormatter) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let RNullValueFields { value_token } = node.as_fields();
+        write![f, [value_token.format()]]
     }
 }
