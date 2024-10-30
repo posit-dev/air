a053ec1517c04ae8fa4284dafa1a02235c79ef93
Davis Vaughan <davis@posit.co>
2024-10-30 13:14:36 -0400

    Add logical literal testing

--- crates/air_r_formatter/src/r/auxiliary/logical_value.rs
+++ crates/air_r_formatter/src/r/auxiliary/logical_value.rs
@@ -1,10 +1,13 @@
 use crate::prelude::*;
 use air_r_syntax::RLogicalValue;
-use biome_rowan::AstNode;
+use air_r_syntax::RLogicalValueFields;
+use biome_formatter::write;
+
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatRLogicalValue;
 impl FormatNodeRule<RLogicalValue> for FormatRLogicalValue {
     fn fmt_fields(&self, node: &RLogicalValue, f: &mut RFormatter) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let RLogicalValueFields { value_token } = node.as_fields();
+        write![f, [value_token.format()]]
     }
 }
