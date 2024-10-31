97b88b177983a0a8ef2a3442889c551d9ec2eed9
Davis Vaughan <davis@posit.co>
2024-10-31 11:57:35 -0400

    Add long signature long body test

--- crates/air_r_formatter/tests/specs/r/function_definition.R
+++ crates/air_r_formatter/tests/specs/r/function_definition.R
@@ -3,6 +3,8 @@ function(a, b) 1
 
 function(a_really_long_argument_name_to_break_on, and_here_is_another_one_please_break_me, and_this) 1
 
+function(a_really_long_argument_name_to_break_on, and_this) a_really_long_argument_name_to_break_on
+
 function() {
   # comment
 }
