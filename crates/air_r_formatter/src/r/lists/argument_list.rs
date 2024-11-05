8d530fa88c22a96248b9ab7ca8bcc33db0f7b02f
Davis Vaughan <davis@posit.co>
2024-11-05 14:39:36 -0500

    Mostly cosmetic rework of the `RArgumentList` rule
    Simplification, allowing for early exits when we see an argument with a leading newline, and adding support for trailing named arguments.

--- crates/air_r_formatter/src/r/lists/argument_list.rs
+++ crates/air_r_formatter/src/r/lists/argument_list.rs
@@ -1,6 +1,8 @@
 use crate::prelude::*;
 use crate::separated::FormatAstSeparatedListExtension;
-use air_r_syntax::{RArgumentList, RBracedExpressions};
+use air_r_syntax::AnyRArgument;
+use air_r_syntax::RArgumentList;
+use air_r_syntax::RSyntaxKind;
 use biome_formatter::separated::TrailingSeparator;
 use biome_formatter::write;
 use biome_rowan::AstSeparatedList;
@@ -12,39 +14,76 @@ impl FormatRule<RArgumentList> for FormatRArgumentList {
     fn fmt(&self, node: &RArgumentList, f: &mut RFormatter) -> FormatResult<()> {
         // TODO: Special handling for comments? See `handle_array_holes` for JS.
 
-        let last_argument_is_braced_expression = node.last().is_some_and(|n| {
-            n.is_ok_and(|n| {
-                let Some(arg) = n.as_r_unnamed_argument() else {
-                    return false;
-                };
-                let Ok(arg) = arg.value() else {
-                    return false;
-                };
-                RBracedExpressions::can_cast(arg.syntax().kind())
-            })
-        });
-
-        let has_leading_newlines = node
-            .iter()
-            .any(|n| n.is_ok_and(|n| n.syntax().has_leading_newline()));
-
-        if !has_leading_newlines && last_argument_is_braced_expression {
-            let formattable = format_with(|f| no_expand_argument_list(node, f));
+        if should_join_arguments_with_space(node)? {
+            let formattable = format_with(|f| join_arguments_with_space(node, f));
             write!(f, [&formattable])
         } else {
-            let formattable = format_with(|f| fully_expand_argument_list(node, f));
+            let formattable = format_with(|f| join_arguments_with_soft_line(node, f));
             write!(f, [group(&soft_block_indent(&formattable))])
         }
     }
 }
 
-fn no_expand_argument_list(node: &RArgumentList, f: &mut RFormatter) -> FormatResult<()> {
+/// Check for a special case consisting of a function call where:
+///
+/// - The last argument is a braced expression
+/// - None of the other arguments break across multiple lines
+///
+/// In this case, we join all arguments with a space, but that's it. The
+/// trailing braced expression will nicely expand over multiple lines as it
+/// formats itself.
+///
+/// ```r
+/// test_that("description", {
+///   1 + 1
+/// })
+///
+/// with(data, {
+///   col1 + col2
+/// })
+/// ```
+///
+/// Forcibly joining with a space means that extremely long argument lists that
+/// end with a braced expression can exceed the line limit, but this is useful
+/// for very long testthat descriptions.
+///
+/// ```r
+/// test_that("a very long and detailed description about the problem that exceeds the limit", {
+///   1 + 1
+/// })
+/// ```
+fn should_join_arguments_with_space(node: &RArgumentList) -> FormatResult<bool> {
+    use AnyRArgument::*;
+    use RSyntaxKind::*;
+
+    for node in node.iter() {
+        if node?.syntax().has_leading_newline() {
+            return Ok(false);
+        }
+    }
+
+    let Some(last) = node.last() else {
+        return Ok(false);
+    };
+
+    // Check if the last argument is a braced expression
+    Ok(match last? {
+        RNamedArgument(node) => match node.value() {
+            None => false,
+            Some(node) => node.syntax().kind() == R_BRACED_EXPRESSIONS,
+        },
+        RUnnamedArgument(node) => node.value()?.syntax().kind() == R_BRACED_EXPRESSIONS,
+        _ => false,
+    })
+}
+
+fn join_arguments_with_space(node: &RArgumentList, f: &mut RFormatter) -> FormatResult<()> {
     let mut joiner = f.join_nodes_with_space();
     join_argument_list(&mut joiner, node)?;
     joiner.finish()
 }
 
-fn fully_expand_argument_list(node: &RArgumentList, f: &mut RFormatter) -> FormatResult<()> {
+fn join_arguments_with_soft_line(node: &RArgumentList, f: &mut RFormatter) -> FormatResult<()> {
     let mut joiner = f.join_nodes_with_soft_line();
     join_argument_list(&mut joiner, node)?;
     joiner.finish()
--- crates/air_r_formatter/tests/specs/r/call.R
+++ crates/air_r_formatter/tests/specs/r/call.R
@@ -2,9 +2,6 @@ fn()
 fn(a)
 fn(a = 1, ... = 2)
 
-# TODO: the `}` should "hug" the `)`, the `a = {` should stay on same line
-fn(a = { 1 + 1 })
-
 # Holes
 fn(,)
 fn(,,)
@@ -20,3 +17,22 @@ fn(a = 1, another_really_really_long_argument_to_test_this_feature, a_really_lon
 test_that("description", {
 	1 + 1
 })
+
+test_that("description that is super long and actually exceeds the line limit but we arent going to break!", {
+	1 + 1
+})
+
+# TODO: This one should probably align the `{` on the opening line
+# (all arguments before the trailing braced expression are on the opening line)
+test_that("description that is super long and actually exceeds the line limit but we arent going to break!",
+{
+	1 + 1
+})
+
+# User broke it manually
+test_that(
+  "description that is super long and actually exceeds the line limit but we arent going to break!", {
+	1 + 1
+})
+
+fn(a = { 1 + 1 })
