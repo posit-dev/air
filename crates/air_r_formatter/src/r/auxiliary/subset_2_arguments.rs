5d9a559e46708f598f06745f26c5645221086597
Davis Vaughan <davis@posit.co>
2024-11-08 14:21:42 -0500

    Push subset/subset2 implementation on top of call implementation

--- crates/air_r_formatter/src/r/auxiliary/subset_2_arguments.rs
+++ crates/air_r_formatter/src/r/auxiliary/subset_2_arguments.rs
@@ -1,4 +1,5 @@
 use crate::prelude::*;
+use crate::r::auxiliary::call_arguments::FormatRCallLikeArguments;
 use air_r_syntax::RSubset2Arguments;
 use air_r_syntax::RSubset2ArgumentsFields;
 use biome_formatter::write;
@@ -7,23 +8,40 @@ use biome_formatter::write;
 pub(crate) struct FormatRSubset2Arguments;
 impl FormatNodeRule<RSubset2Arguments> for FormatRSubset2Arguments {
     fn fmt_fields(&self, node: &RSubset2Arguments, f: &mut RFormatter) -> FormatResult<()> {
+        // TODO: Special handling for comments? See `handle_array_holes` for JS.
+
         let RSubset2ArgumentsFields {
             l_brack2_token,
             items,
             r_brack2_token,
         } = node.as_fields();
 
-        // TODO: Rebase on top of `call_arguments.rs` reworking, probably remove
-        // the `group(soft_block_indent())` in favor of it happening internally?
-        // Or possibly we will need a special `FormatRCallArgumentList` formatter
-        // to differentiate call `items` from subset or subset2 `items`.
-        write!(
-            f,
-            [
-                l_brack2_token.format(),
-                group(&soft_block_indent(&items.format())),
-                r_brack2_token.format()
-            ]
-        )
+        // Special case where the dangling comment has no node to attach to:
+        //
+        // ```r
+        // x[[
+        //   # dangling comment
+        // ]]
+        // ```
+        //
+        // If we don't handle it specially, it can break idempotence
+        if items.is_empty() {
+            return write!(
+                f,
+                [
+                    l_brack2_token.format(),
+                    format_dangling_comments(node.syntax()).with_soft_block_indent(),
+                    r_brack2_token.format()
+                ]
+            );
+        }
+
+        FormatRCallLikeArguments::new(l_brack2_token, items, r_brack2_token).fmt(f)
+    }
+
+    fn fmt_dangling_comments(&self, _: &RSubset2Arguments, _: &mut RFormatter) -> FormatResult<()> {
+        // Formatted inside of `fmt_fields`
+        // Only applicable for the empty arguments case
+        Ok(())
     }
 }
