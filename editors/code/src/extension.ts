6bb5abc1e09a602f76da651e2b13e213e266bece
Lionel Henry <lionel.hry@proton.me>
2024-11-18 14:06:42 +0100

    Draft VSCode/Positron extension

--- editors/code/src/extension.ts
+++ editors/code/src/extension.ts
@@ -1,26 +1,88 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+} from "vscode-languageclient/node";
+
+// The LSP is stored in this global singleton
+let client: LanguageClient | undefined;
+
+// Simple flags to manage the state of the LSP
+let restartInProgress = false;
+let stopInProgress = false;
+
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "air" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('air.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from air!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand("air.restart", async () => {
+			await restartAirLsp();
+		}),
+	);
+	startAirLsp();
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	stopAirLsp();
+}
+
+export async function startAirLsp() {
+	if (client) {
+		throw new Error("Air is already running");
+	}
+
+	let options: ServerOptions = {
+		command: "air",
+		args: ["lsp"],
+	};
+
+	let clientOptions: LanguageClientOptions = {
+		documentSelector: [{ scheme: "file", language: "r" }],
+		synchronize: {
+			// Notify the server about file changes to R files contained in the workspace
+			fileEvents: vscode.workspace.createFileSystemWatcher("**/*.[Rr]"),
+		},
+	};
+
+	client = new LanguageClient(
+		"airLanguageServer",
+		"Air Language Server",
+		options,
+		clientOptions,
+	);
+	await client.start();
+}
+
+export async function stopAirLsp() {
+	if (!client) {
+		throw new Error("Air is not running");
+	}
+
+	if (stopInProgress) {
+		throw new Error("Air is already restarting");
+	}
+	stopInProgress = true;
+
+	try {
+		await client.stop();
+		client = undefined;
+	} finally {
+		stopInProgress = false;
+	}
+}
+
+export async function restartAirLsp() {
+	if (restartInProgress) {
+		throw new Error("Air is already restarting");
+	}
+	restartInProgress = true;
+
+	try {
+		if (client) {
+			await client.stop();
+		}
+		await startAirLsp();
+	} finally {
+		restartInProgress = false;
+	}
+}
