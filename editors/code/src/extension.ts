03d013e58eb8e15dad5d17fbe2fe3ce6c35cc854
Lionel Henry <lionel.hry@proton.me>
2024-11-20 09:30:37 +0100

    Extract `Lsp` class

--- editors/code/src/extension.ts
+++ editors/code/src/extension.ts
@@ -1,97 +1,19 @@
 import * as vscode from "vscode";
+import { Lsp } from "./lsp";
 
-import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-} from "vscode-languageclient/node";
-
-// The LSP is stored in this global singleton
-let client: LanguageClient | undefined;
-
-// We use the same output channel for all LSP instances (e.g. a new instance
-// after a restart) to avoid having multiple channels in the Output viewpane.
-let channel: vscode.OutputChannel;
-
-// Simple flags to manage the state of the LSP
-let restartInProgress = false;
-let stopInProgress = false;
+let lsp: Lsp;
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand("air.restart", async () => {
-			await restartAirLsp();
+			await lsp.restart();
 		}),
 	);
 
-	channel = vscode.window.createOutputChannel("Air Language Server");
-	context.subscriptions.push(channel);
-
-	startAirLsp();
+	lsp = new Lsp(context);
+	lsp.start();
 }
 
 export function deactivate() {
-	stopAirLsp();
-}
-
-export async function startAirLsp() {
-	if (client) {
-		throw new Error("Air is already running");
-	}
-
-	let options: ServerOptions = {
-		command: "air",
-		args: ["lsp"],
-	};
-
-	let clientOptions: LanguageClientOptions = {
-		documentSelector: [{ scheme: "file", language: "r" }],
-		synchronize: {
-			// Notify the server about file changes to R files contained in the workspace
-			fileEvents: vscode.workspace.createFileSystemWatcher("**/*.[Rr]"),
-		},
-		outputChannel: channel,
-	};
-
-	client = new LanguageClient(
-		"airLanguageServer",
-		"Air Language Server",
-		options,
-		clientOptions,
-	);
-	await client.start();
-}
-
-export async function stopAirLsp() {
-	if (!client) {
-		throw new Error("Air is not running");
-	}
-
-	if (stopInProgress) {
-		throw new Error("Air is already restarting");
-	}
-	stopInProgress = true;
-
-	try {
-		await client.stop();
-	} finally {
-		client = undefined;
-		stopInProgress = false;
-	}
-}
-
-export async function restartAirLsp() {
-	if (restartInProgress) {
-		throw new Error("Air is already restarting");
-	}
-	restartInProgress = true;
-
-	try {
-		if (client) {
-			await stopAirLsp();
-		}
-		await startAirLsp();
-	} finally {
-		restartInProgress = false;
-	}
+	lsp.stop();
 }
---
+++ editors/code/src/lsp.ts
@@ -0,0 +1,86 @@
+import * as vscode from "vscode";
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+} from "vscode-languageclient/node";
+
+export class Lsp {
+	private client: LanguageClient | null = null;
+
+	// We use the same output channel for all LSP instances (e.g. a new instance
+	// after a restart) to avoid having multiple channels in the Output viewpane.
+	private channel: vscode.OutputChannel;
+
+	// Simple flags to manage the state of the LSP
+	private restartInProgress = false;
+	private stopInProgress = false;
+
+	constructor(context: vscode.ExtensionContext) {
+		this.channel = vscode.window.createOutputChannel("Air Language Server");
+		context.subscriptions.push(this.channel);
+	}
+
+	public async start() {
+		if (this.client) {
+			throw new Error("Air is already running");
+		}
+
+		let options: ServerOptions = {
+			command: "air",
+			args: ["lsp"],
+		};
+
+		let clientOptions: LanguageClientOptions = {
+			documentSelector: [{ scheme: "file", language: "r" }],
+			synchronize: {
+				// Notify the server about file changes to R files contained in the workspace
+				fileEvents:
+					vscode.workspace.createFileSystemWatcher("**/*.[Rr]"),
+			},
+			outputChannel: this.channel,
+		};
+
+		this.client = new LanguageClient(
+			"airLanguageServer",
+			"Air Language Server",
+			options,
+			clientOptions,
+		);
+		await this.client.start();
+	}
+
+	public async stop() {
+		if (!this.client) {
+			throw new Error("Air is not running");
+		}
+
+		if (this.stopInProgress) {
+			throw new Error("Air is already restarting");
+		}
+		this.stopInProgress = true;
+
+		try {
+			await this.client.stop();
+		} finally {
+			this.client = null;
+			this.stopInProgress = false;
+		}
+	}
+
+	public async restart() {
+		if (this.restartInProgress) {
+			throw new Error("Air is already restarting");
+		}
+		this.restartInProgress = true;
+
+		try {
+			if (this.client) {
+				await this.stop();
+			}
+			await this.start();
+		} finally {
+			this.restartInProgress = false;
+		}
+	}
+}
