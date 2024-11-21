3837f76ae922b2385ca193817f8b4b1b356b8b84
Lionel Henry <lionel.hry@proton.me>
2024-11-21 19:09:17 +0100

    Add command to save file representations as zip

--- editors/code/src/commands.ts
+++ editors/code/src/commands.ts
@@ -1,7 +1,10 @@
 import * as vscode from "vscode";
+import AdmZip from "adm-zip";
+
 import { Cmd, Ctx } from "./context";
 import { viewFileUsingTextDocumentContentProvider } from "./rust-analyzer/viewFileProvider";
 import * as ext from "./lsp-ext";
+import path from "path";
 
 export function registerCommands(ctx: Ctx) {
 	ctx.extension.subscriptions.push(
@@ -10,24 +13,28 @@ export function registerCommands(ctx: Ctx) {
 			async () => await ctx.lsp.restart(),
 		),
 	);
+
 	ctx.extension.subscriptions.push(
 		vscode.commands.registerCommand(
 			"air.viewSyntaxTree",
 			viewSyntaxTree(ctx),
 		),
 	);
+
 	ctx.extension.subscriptions.push(
 		vscode.commands.registerCommand(
 			"air.viewSyntaxTreeTs",
 			viewSyntaxTreeTs(ctx),
 		),
 	);
+
 	ctx.extension.subscriptions.push(
 		vscode.commands.registerCommand(
 			"air.viewFormatTree",
 			viewFormatTree(ctx),
 		),
 	);
+
 	ctx.extension.subscriptions.push(
 		vscode.commands.registerCommand(
 			"air.viewFileRepresentations",
@@ -35,6 +42,18 @@ export function registerCommands(ctx: Ctx) {
 				await vscode.commands.executeCommand("air.viewSyntaxTree");
 				await vscode.commands.executeCommand("air.viewSyntaxTreeTs");
 				await vscode.commands.executeCommand("air.viewFormatTree");
+			},
+		),
+	);
+
+	ctx.extension.subscriptions.push(
+		vscode.commands.registerCommand(
+			"air.saveFileRepresentations",
+			async () => {
+				await vscode.commands.executeCommand(
+					"air.viewFileRepresentations",
+				);
+				zipFileRepresentations();
 			},
 		),
 	);
@@ -80,4 +99,41 @@ function viewFormatTree(ctx: Ctx): Cmd {
 		scheme,
 		true,
 	);
+}
+
+async function zipFileRepresentations() {
+	const docs = vscode.workspace.textDocuments.filter((doc) => {
+		switch (doc.uri.scheme) {
+			case "air-syntax-tree":
+			case "air-syntax-tree-ts":
+			case "air-format-tree":
+				return true;
+			default:
+				return false;
+		}
+	});
+
+	const zip = new AdmZip();
+
+	for (const doc of docs) {
+		const name = path.basename(doc.fileName);
+		const content = doc.getText();
+		zip.addFile(name, Buffer.from(content, "utf8"));
+	}
+
+	const uri = await vscode.window.showSaveDialog({
+		filters: {
+			"Zip files": ["zip"],
+		},
+		defaultUri: vscode.Uri.file(
+			path.join(
+				vscode.workspace.rootPath || __dirname,
+				"file-representations.zip",
+			),
+		),
+	});
+
+	if (uri) {
+		zip.writeZip(uri.fsPath);
+	}
 }
