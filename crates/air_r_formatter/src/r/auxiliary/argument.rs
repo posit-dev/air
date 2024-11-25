dfe683c30c982c5a11002b84b087cc7526f4a32e
Davis Vaughan <davis@posit.co>
2024-11-25 15:59:28 -0500

    Add `RArgument` and `RArgumentNameClause` format rules

---
+++ crates/air_r_formatter/src/r/auxiliary/argument.rs
@@ -0,0 +1,35 @@
+use crate::prelude::*;
+use air_r_syntax::RArgument;
+use air_r_syntax::RArgumentFields;
+use biome_formatter::write;
+
+#[derive(Debug, Clone, Default)]
+pub(crate) struct FormatRArgument;
+impl FormatNodeRule<RArgument> for FormatRArgument {
+    fn fmt_fields(&self, node: &RArgument, f: &mut RFormatter) -> FormatResult<()> {
+        let RArgumentFields { name_clause, value } = node.as_fields();
+
+        match (name_clause, value) {
+            // Hole
+            (None, None) => Ok(()),
+
+            // Unnamed argument
+            // `foo(value)`
+            // `foo(value, value)`
+            (None, Some(value)) => write!(f, [value.format()]),
+
+            // Named argument without a value
+            // We write a mandatory space as a signal that this is a fairly
+            // weird a nonstandard thing to see.
+            // `foo(name = )`
+            // `foo(name = , value)`
+            (Some(name_clause), None) => write!(f, [name_clause.format(), space()]),
+
+            // Named argument with a value
+            // `foo(name = value)`
+            (Some(name_clause), Some(value)) => {
+                write!(f, [name_clause.format(), space(), value.format()])
+            }
+        }
+    }
+}
