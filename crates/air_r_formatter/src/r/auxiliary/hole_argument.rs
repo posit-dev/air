451375e0fec12bf86f2249f6005b969cc90bf67c
Davis Vaughan <davis@posit.co>
2024-11-25 15:58:54 -0500

    Push most generated formatter files

--- crates/air_r_formatter/src/r/auxiliary/hole_argument.rs
+++
@@ -1,9 +0,0 @@
-use crate::prelude::*;
-use air_r_syntax::RHoleArgument;
-#[derive(Debug, Clone, Default)]
-pub(crate) struct FormatRHoleArgument;
-impl FormatNodeRule<RHoleArgument> for FormatRHoleArgument {
-    fn fmt_fields(&self, _node: &RHoleArgument, _f: &mut RFormatter) -> FormatResult<()> {
-        Ok(())
-    }
-}
