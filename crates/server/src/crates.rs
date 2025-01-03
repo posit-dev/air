cf6715a2a2210b7ddbac1b73ee54069e06a6d0b2
Davis Vaughan <davis@posit.co>
2025-01-03 10:02:58 -0500

    Add a test for `AIR_CRATE_NAMES`

--- crates/server/src/crates.rs
+++ crates/server/src/crates.rs
@@ -1,3 +1,13 @@
 // Generates `AIR_CRATE_NAMES`, a const array of the crate names in the air workspace,
 // see `server/src/build.rs`
 include!(concat!(env!("OUT_DIR"), "/crates.rs"));
+
+#[cfg(test)]
+mod tests {
+    use crate::crates::AIR_CRATE_NAMES;
+
+    #[test]
+    fn test_crate_names() {
+        insta::assert_debug_snapshot!(AIR_CRATE_NAMES);
+    }
+}
