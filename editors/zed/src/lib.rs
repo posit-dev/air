bd92ae901600df927337413435a68c014d38241d
Davis Vaughan <davis@posit.co>
2025-02-25 10:41:43 -0500

    Extract out testable `GithubReleaseDetails`

--- editors/zed/src/lib.rs
+++ editors/zed/src/lib.rs
@@ -11,6 +11,23 @@ struct AirExtension {
     cached_binary_path: Option<String>,
 }
 
+#[derive(Debug, PartialEq)]
+struct GithubReleaseDetails {
+    /// The name of the GitHub asset that contains the binary
+    asset_name: String,
+
+    /// The type of file the asset is compressed as
+    downloaded_file_type: zed::DownloadedFileType,
+
+    /// The on disk directory the asset is extracted into, relative to the extension's
+    /// working directory
+    downloaded_directory: String,
+
+    /// The on disk path to the binary. This is nested within `downloaded_directory`,
+    /// and is relative to the extension's working directory.
+    downloaded_binary_path: String,
+}
+
 impl AirExtension {
     fn language_server_binary(
         &mut self,
@@ -72,7 +89,64 @@ impl AirExtension {
         )?;
 
         let (platform, arch) = zed::current_platform();
+        let release_details = GithubReleaseDetails::new(platform, arch, release.version);
 
+        let asset = release
+            .assets
+            .iter()
+            .find(|asset| asset.name == release_details.asset_name)
+            .ok_or_else(|| {
+                format!(
+                    "No asset found matching {asset_name:?}",
+                    asset_name = release_details.asset_name
+                )
+            })?;
+
+        if !fs::metadata(&release_details.downloaded_binary_path)
+            .map_or(false, |stat| stat.is_file())
+        {
+            zed::set_language_server_installation_status(
+                language_server_id,
+                &zed::LanguageServerInstallationStatus::Downloading,
+            );
+
+            zed::download_file(
+                &asset.download_url,
+                &release_details.downloaded_directory,
+                release_details.downloaded_file_type,
+            )
+            .map_err(|error| format!("Failed to download file: {error}"))?;
+
+            // Clean out other entries in our personal extension directory, this may
+            // include outdated versions of the extension, so it is good hygiene
+            let entries = fs::read_dir(".")
+                .map_err(|error| format!("Failed to list working directory: {error}"))?;
+
+            for entry in entries {
+                let entry =
+                    entry.map_err(|error| format!("Failed to load directory entry: {error}"))?;
+                if entry.file_name().to_str() != Some(&release_details.downloaded_directory) {
+                    fs::remove_dir_all(entry.path()).ok();
+                }
+            }
+        }
+
+        // Update cache path for later
+        self.cached_binary_path = Some(release_details.downloaded_binary_path.clone());
+
+        Ok(AirBinary {
+            path: release_details.downloaded_binary_path,
+            args: binary_args,
+        })
+    }
+}
+
+impl GithubReleaseDetails {
+    fn new(
+        platform: zed_extension_api::Os,
+        arch: zed_extension_api::Architecture,
+        version: String,
+    ) -> Self {
         let asset_stem = format!(
             "air-{arch}-{os}",
             arch = match arch {
@@ -86,6 +160,7 @@ impl AirExtension {
                 zed::Os::Windows => "pc-windows-msvc",
             }
         );
+
         let asset_name = format!(
             "{asset_stem}.{suffix}",
             suffix = match platform {
@@ -94,53 +169,24 @@ impl AirExtension {
             }
         );
 
-        let asset = release
-            .assets
-            .iter()
-            .find(|asset| asset.name == asset_name)
-            .ok_or_else(|| format!("No asset found matching {asset_name:?}"))?;
-
-        let version_dir = format!("air-{}", release.version);
-        let binary_path = match platform {
-            zed::Os::Mac | zed::Os::Linux => format!("{version_dir}/{asset_stem}/air"),
-            zed::Os::Windows => format!("{version_dir}/air.exe"),
+        let downloaded_file_type = match platform {
+            zed::Os::Mac | zed::Os::Linux => zed::DownloadedFileType::GzipTar,
+            zed::Os::Windows => zed::DownloadedFileType::Zip,
         };
 
-        if !fs::metadata(&binary_path).map_or(false, |stat| stat.is_file()) {
-            zed::set_language_server_installation_status(
-                language_server_id,
-                &zed::LanguageServerInstallationStatus::Downloading,
-            );
+        let downloaded_directory = format!("air-{version}");
 
-            let file_kind = match platform {
-                zed::Os::Mac | zed::Os::Linux => zed::DownloadedFileType::GzipTar,
-                zed::Os::Windows => zed::DownloadedFileType::Zip,
-            };
+        let downloaded_binary_path = match platform {
+            zed::Os::Mac | zed::Os::Linux => format!("{downloaded_directory}/{asset_stem}/air"),
+            zed::Os::Windows => format!("{downloaded_directory}/air.exe"),
+        };
 
-            zed::download_file(&asset.download_url, &version_dir, file_kind)
-                .map_err(|error| format!("Failed to download file: {error}"))?;
-
-            // Clean out other entries in our personal extension directory, this may
-            // include outdated versions of the extension, so it is good hygiene
-            let entries = fs::read_dir(".")
-                .map_err(|error| format!("Failed to list working directory: {error}"))?;
-
-            for entry in entries {
-                let entry =
-                    entry.map_err(|error| format!("Failed to load directory entry: {error}"))?;
-                if entry.file_name().to_str() != Some(&version_dir) {
-                    fs::remove_dir_all(entry.path()).ok();
-                }
-            }
+        Self {
+            asset_name,
+            downloaded_file_type,
+            downloaded_directory,
+            downloaded_binary_path,
         }
-
-        // Update cache path for later
-        self.cached_binary_path = Some(binary_path.clone());
-
-        Ok(AirBinary {
-            path: binary_path,
-            args: binary_args,
-        })
     }
 }
 
@@ -192,3 +238,54 @@ impl zed::Extension for AirExtension {
 }
 
 zed::register_extension!(AirExtension);
+
+#[cfg(test)]
+mod test {
+    use crate::GithubReleaseDetails;
+
+    /// Tests a few important variants across OSes and Arches
+    #[test]
+    fn test_github_release_details() {
+        assert_eq!(
+            GithubReleaseDetails::new(
+                zed_extension_api::Os::Mac,
+                zed_extension_api::Architecture::Aarch64,
+                String::from("0.1.0"),
+            ),
+            GithubReleaseDetails {
+                asset_name: String::from("air-aarch64-apple-darwin.tar.gz"),
+                downloaded_file_type: zed_extension_api::DownloadedFileType::GzipTar,
+                downloaded_directory: String::from("air-0.1.0"),
+                downloaded_binary_path: String::from("air-0.1.0/air-aarch64-apple-darwin/air")
+            }
+        );
+
+        assert_eq!(
+            GithubReleaseDetails::new(
+                zed_extension_api::Os::Linux,
+                zed_extension_api::Architecture::X8664,
+                String::from("0.2.0"),
+            ),
+            GithubReleaseDetails {
+                asset_name: String::from("air-x86_64-unknown-linux-gnu.tar.gz"),
+                downloaded_file_type: zed_extension_api::DownloadedFileType::GzipTar,
+                downloaded_directory: String::from("air-0.2.0"),
+                downloaded_binary_path: String::from("air-0.2.0/air-x86_64-unknown-linux-gnu/air")
+            }
+        );
+
+        assert_eq!(
+            GithubReleaseDetails::new(
+                zed_extension_api::Os::Windows,
+                zed_extension_api::Architecture::X86,
+                String::from("0.1.0"),
+            ),
+            GithubReleaseDetails {
+                asset_name: String::from("air-x86-pc-windows-msvc.zip"),
+                downloaded_file_type: zed_extension_api::DownloadedFileType::Zip,
+                downloaded_directory: String::from("air-0.1.0"),
+                downloaded_binary_path: String::from("air-0.1.0/air.exe")
+            }
+        );
+    }
+}
