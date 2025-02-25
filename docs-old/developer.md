88c573ba9cdabf084cf7817ab419f87beea37b39
Davis Vaughan <davis@posit.co>
2025-02-25 11:23:28 -0500

    Experimental Zed extension for Air (#195)
    * Draft Zed extension for Air
    
    * Fine tuning of the zed extension
    
    * Snapshot update
    
    * Extract out testable `GithubReleaseDetails`
    
    * Tweak `name`
    
    * Add Zed release process and installation docs

--- docs-old/developer.md
+++ docs-old/developer.md
@@ -1,50 +1,76 @@
 # Release
 
-The release process of the air cli has some manual steps. One complication is that for each release of the CLI binary, we create a new release of the extension as this is our primary way of distributing Air. The version numbers between the CLI binary and the extension are not required to be the same.
+The release process of Air has some manual steps. One complication is that for each release of the CLI binary, we create a new release of the VS Code extension as this is our primary way of distributing Air. The version numbers between the CLI binary and the VS Code extension are not required to be the same.
 
 When you want to cut a release:
 
-- Create a release branch
+-   Create a release branch
 
-    - Polish `CHANGELOG.md`, bump the version and add a new `Development version` header (yep, right away - `cargo dist` is smart enough to ignore this header).
+    -   Polish `CHANGELOG.md`, bump the version and add a new `Development version` header (yep, right away - `cargo dist` is smart enough to ignore this header).
 
-    - Polish `editors/code/CHANGELOG.md`, bump the version and add a new `Development version` header.
+    -   Polish `editors/code/CHANGELOG.md`, bump the version and add a new `Development version` header.
 
-    - In `crates/air/Cargo.toml`, bump the version.
+    -   In `crates/air/Cargo.toml`, bump the version.
 
-    - Run `cargo check` to sync `Cargo.lock`.
+    -   Run `cargo check` to sync `Cargo.lock`.
 
-    - In `editors/code/package.json`, bump the minor version to the next even number for standard releases, or to the next odd number for preview releases.
+    -   In `editors/code/package.json`, bump the minor version to the next even number for standard releases, or to the next odd number for preview releases.
 
-    - Open a PR with these changes.
+    -   Open a PR with these changes.
 
-- Manually run the [release workflow](https://github.com/posit-dev/air/actions/workflows/release.yml)
+-   Manually run the [release workflow](https://github.com/posit-dev/air/actions/workflows/release.yml)
 
-    - It runs on `workflow_dispatch`, and you must provide the `Release Tag` version to create. Always provide the same version that you used in `Cargo.toml`. Do not prefix the version with a `v`.
+    -   It runs on `workflow_dispatch`, and you must provide the `Release Tag` version to create. Always provide the same version that you used in `Cargo.toml`. Do not prefix the version with a `v`.
 
-    - The release workflow will:
+    -   The release workflow will:
 
-        - Build the binaries and installer scripts.
+        -   Build the binaries and installer scripts.
 
-        - Create and push a git tag for the version.
+        -   Create and push a git tag for the version.
 
-        - Create a GitHub Release attached to that git tag.
+        -   Create a GitHub Release attached to that git tag.
 
-        - Attach the binaries and scripts to that GitHub Release as artifacts.
+        -   Attach the binaries and scripts to that GitHub Release as artifacts.
 
-- Manually run the [extension release workflow](https://github.com/posit-dev/air/actions/workflows/release-vscode.yml)
+-   Manually run the [extension release workflow](https://github.com/posit-dev/air/actions/workflows/release-vscode.yml)
 
-    - It runs on `workflow_dispatch`, and automatically pulls in the latest release binary of Air from the binary release workflow above.
+    -   It runs on `workflow_dispatch`, and automatically pulls in the latest release binary of Air from the binary release workflow above.
 
-- Merge the release branch
+-   Merge the release branch
 
-    - There is no need to bump to an intermediate "dev version" after a release.
+    -   There is no need to bump to an intermediate "dev version" after a release.
 
-# Development installation
+# Release Zed extension
+
+It is rare to need to do a Zed extension release because that code is fairly static. It knows how to download the latest version of Air, so we only need to change something there if we alter the way the extension itself works.
+
+For a new release:
+
+-   Create a release branch in Air called `release-zed/x.y.z`
+
+    -   Update the `version` in `editors/zed/Cargo.toml`.
+
+    -   Update the `version` in `editors/zed/extension.toml` to the same version.
+
+    -   Open a PR with these changes, and go ahead and squash merge the PR. Note the commit hash of the merged commit.
+
+-   Fork the [`zed-industries/extensions`](https://github.com/zed-industries/extensions) repository if you haven't yet.
+
+-   Create a release branch in `zed-industries/extensions` called `update-air/x.y.z`
+
+    -   Update the Air submodule in `extensions/` to point to the commit of the newly merged PR from the Air Zed extension release.
+
+    -   Update the Air `version` in `extensions.toml`. Double check that this `version` matches the `version` set in the Air Zed extension.
+
+    -   Do a PR to `zed-industries/extensions` with these changes.
+
+If you have any questions about the process, refer to [Zed's update guide](https://zed.dev/docs/extensions/developing-extensions#updating-an-extension).
+
+# VS Code Extension development installation
 
 Install the dev version of the Air cli with:
 
-```sh
+``` sh
 cargo install --path crates/air --debug
 ```
 
@@ -52,7 +78,7 @@ This installs it to `~/.cargo/bin` (which must be on your `PATH`), and can be re
 
 Install the dev version of the VS Code extension:
 
-```sh
+``` sh
 # Install for Positron
 (cd editors/code && (rm -rf *.vsix || true) && npx @vscode/vsce package && positron --install-extension *.vsix)
 
@@ -62,16 +88,30 @@ Install the dev version of the VS Code extension:
 
 The CLI tools for Positron or VS Code need to be installed on your path using the command palette command `Shell Command: Install 'code'/'positron' command in PATH`.
 
+# Zed Extension development installation
+
+Zed has a great guide on [developing extensions](https://zed.dev/docs/extensions/developing-extensions) if you are working on the development version of the Air Zed extension. Read that first.
+
+To install the development version of the Air extension:
+
+-   Run `zed: install dev extension`
+
+-   Select the `editors/zed` directory. This will install the development version of the Air extension. An `extension.wasm` file should be created in the `editors/zed` directory. This file is `.gitignore`d.
+
+To rebuild the extension, Zed has a shortcut process:
+
+-   Run `zed: extensions`
+
+-   Find `Air` in the list
+
+-   Click `Rebuild`
+
 # Testing
 
-We use [nextest](https://nexte.st/) for testing rather than a standard `cargo test`, primarily because nextest runs each test in its own process rather than in its own thread.
-This is critical for us, as Air has global objects that can only be set up once per process (such as the global logger).
-Additionally, using one process per test means that it is impossible for one test to interfere with another (so you don't have to worry about test cleanup).
-Tests are still run in parallel, using multiple processes, and this ends up being quite fast and reliable.
+We use [nextest](https://nexte.st/) for testing rather than a standard `cargo test`, primarily because nextest runs each test in its own process rather than in its own thread. This is critical for us, as Air has global objects that can only be set up once per process (such as the global logger). Additionally, using one process per test means that it is impossible for one test to interfere with another (so you don't have to worry about test cleanup). Tests are still run in parallel, using multiple processes, and this ends up being quite fast and reliable.
 
 Install the nextest cli tool using a [prebuilt binary](https://nexte.st/docs/installation/pre-built-binaries/).
 
-Run tests locally with `just test`, which calls `cargo nextest run`.
-Run insta snapshot tests in "update" mode with `just test-insta`.
+Run tests locally with `just test`, which calls `cargo nextest run`. Run insta snapshot tests in "update" mode with `just test-insta`.
 
 On CI we use the nextest profile found in `.config/nextest.toml`.
---
+++ editors/zed/src/lib.rs
@@ -0,0 +1,291 @@
+use std::fs;
+use zed::LanguageServerId;
+use zed_extension_api::{self as zed, settings::LspSettings, Result};
+
+struct AirBinary {
+    path: String,
+    args: Option<Vec<String>>,
+}
+
+struct AirExtension {
+    cached_binary_path: Option<String>,
+}
+
+#[derive(Debug, PartialEq)]
+struct GithubReleaseDetails {
+    /// The name of the GitHub asset that contains the binary
+    asset_name: String,
+
+    /// The type of file the asset is compressed as
+    downloaded_file_type: zed::DownloadedFileType,
+
+    /// The on disk directory the asset is extracted into, relative to the extension's
+    /// working directory
+    downloaded_directory: String,
+
+    /// The on disk path to the binary. This is nested within `downloaded_directory`,
+    /// and is relative to the extension's working directory.
+    downloaded_binary_path: String,
+}
+
+impl AirExtension {
+    fn language_server_binary(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<AirBinary> {
+        // Pull `BinarySettings`, if they exist. This includes user specified path to the
+        // binary and any user specified arguments for the binary.
+        let binary_settings = LspSettings::for_worktree(language_server_id.as_ref(), worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.binary);
+
+        // We pass through user specified binary arguments no matter what method is
+        // used to get the binary. If no arguments are supplied we eventually fall back to
+        // just `language-server` as the sole argument.
+        let binary_args = binary_settings
+            .as_ref()
+            .and_then(|binary_settings| binary_settings.arguments.clone());
+
+        // Use user specified path to the `air` binary, if it is specified
+        if let Some(path) = binary_settings.and_then(|binary_settings| binary_settings.path) {
+            return Ok(AirBinary {
+                path,
+                args: binary_args,
+            });
+        }
+
+        // Use binary on the `PATH`, if it exists
+        if let Some(path) = worktree.which("air") {
+            return Ok(AirBinary {
+                path,
+                args: binary_args,
+            });
+        }
+
+        // Use binary from a previous download, if we can find one
+        // (I'm not sure if this is cached across Zed sessions, or just within a single
+        // Zed session when the server restarts)
+        if let Some(path) = &self.cached_binary_path {
+            if fs::metadata(path).map_or(false, |stat| stat.is_file()) {
+                return Ok(AirBinary {
+                    path: path.clone(),
+                    args: binary_args,
+                });
+            }
+        }
+
+        // Ok, all other methods failed, we need to download the binary
+        zed::set_language_server_installation_status(
+            language_server_id,
+            &zed::LanguageServerInstallationStatus::CheckingForUpdate,
+        );
+        let release = zed::latest_github_release(
+            "posit-dev/air",
+            zed::GithubReleaseOptions {
+                require_assets: true,
+                pre_release: false,
+            },
+        )?;
+
+        let (platform, arch) = zed::current_platform();
+        let release_details = GithubReleaseDetails::new(platform, arch, release.version);
+
+        let asset = release
+            .assets
+            .iter()
+            .find(|asset| asset.name == release_details.asset_name)
+            .ok_or_else(|| {
+                format!(
+                    "No asset found matching {asset_name:?}",
+                    asset_name = release_details.asset_name
+                )
+            })?;
+
+        if !fs::metadata(&release_details.downloaded_binary_path)
+            .map_or(false, |stat| stat.is_file())
+        {
+            zed::set_language_server_installation_status(
+                language_server_id,
+                &zed::LanguageServerInstallationStatus::Downloading,
+            );
+
+            zed::download_file(
+                &asset.download_url,
+                &release_details.downloaded_directory,
+                release_details.downloaded_file_type,
+            )
+            .map_err(|error| format!("Failed to download file: {error}"))?;
+
+            // Clean out other entries in our personal extension directory, this may
+            // include outdated versions of the extension, so it is good hygiene
+            let entries = fs::read_dir(".")
+                .map_err(|error| format!("Failed to list working directory: {error}"))?;
+
+            for entry in entries {
+                let entry =
+                    entry.map_err(|error| format!("Failed to load directory entry: {error}"))?;
+                if entry.file_name().to_str() != Some(&release_details.downloaded_directory) {
+                    fs::remove_dir_all(entry.path()).ok();
+                }
+            }
+        }
+
+        // Update cache path for later
+        self.cached_binary_path = Some(release_details.downloaded_binary_path.clone());
+
+        Ok(AirBinary {
+            path: release_details.downloaded_binary_path,
+            args: binary_args,
+        })
+    }
+}
+
+impl GithubReleaseDetails {
+    fn new(
+        platform: zed_extension_api::Os,
+        arch: zed_extension_api::Architecture,
+        version: String,
+    ) -> Self {
+        let asset_stem = format!(
+            "air-{arch}-{os}",
+            arch = match arch {
+                zed::Architecture::Aarch64 => "aarch64",
+                zed::Architecture::X86 => "x86",
+                zed::Architecture::X8664 => "x86_64",
+            },
+            os = match platform {
+                zed::Os::Mac => "apple-darwin",
+                zed::Os::Linux => "unknown-linux-gnu",
+                zed::Os::Windows => "pc-windows-msvc",
+            }
+        );
+
+        let asset_name = format!(
+            "{asset_stem}.{suffix}",
+            suffix = match platform {
+                zed::Os::Mac | zed::Os::Linux => "tar.gz",
+                zed::Os::Windows => "zip",
+            }
+        );
+
+        let downloaded_file_type = match platform {
+            zed::Os::Mac | zed::Os::Linux => zed::DownloadedFileType::GzipTar,
+            zed::Os::Windows => zed::DownloadedFileType::Zip,
+        };
+
+        let downloaded_directory = format!("air-{version}");
+
+        let downloaded_binary_path = match platform {
+            zed::Os::Mac | zed::Os::Linux => format!("{downloaded_directory}/{asset_stem}/air"),
+            zed::Os::Windows => format!("{downloaded_directory}/air.exe"),
+        };
+
+        Self {
+            asset_name,
+            downloaded_file_type,
+            downloaded_directory,
+            downloaded_binary_path,
+        }
+    }
+}
+
+impl zed::Extension for AirExtension {
+    fn new() -> Self {
+        Self {
+            cached_binary_path: None,
+        }
+    }
+
+    fn language_server_command(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<zed::Command> {
+        let air_binary = self.language_server_binary(language_server_id, worktree)?;
+        Ok(zed::Command {
+            command: air_binary.path,
+            args: air_binary
+                .args
+                .unwrap_or_else(|| vec!["language-server".into()]),
+            env: vec![],
+        })
+    }
+
+    fn language_server_initialization_options(
+        &mut self,
+        server_id: &LanguageServerId,
+        worktree: &zed_extension_api::Worktree,
+    ) -> Result<Option<zed_extension_api::serde_json::Value>> {
+        let settings = LspSettings::for_worktree(server_id.as_ref(), worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.initialization_options.clone())
+            .unwrap_or_default();
+        Ok(Some(settings))
+    }
+
+    fn language_server_workspace_configuration(
+        &mut self,
+        server_id: &LanguageServerId,
+        worktree: &zed_extension_api::Worktree,
+    ) -> Result<Option<zed_extension_api::serde_json::Value>> {
+        let settings = LspSettings::for_worktree(server_id.as_ref(), worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.settings.clone())
+            .unwrap_or_default();
+        Ok(Some(settings))
+    }
+}
+
+zed::register_extension!(AirExtension);
+
+#[cfg(test)]
+mod test {
+    use crate::GithubReleaseDetails;
+
+    /// Tests a few important variants across OSes and Arches
+    #[test]
+    fn test_github_release_details() {
+        assert_eq!(
+            GithubReleaseDetails::new(
+                zed_extension_api::Os::Mac,
+                zed_extension_api::Architecture::Aarch64,
+                String::from("0.1.0"),
+            ),
+            GithubReleaseDetails {
+                asset_name: String::from("air-aarch64-apple-darwin.tar.gz"),
+                downloaded_file_type: zed_extension_api::DownloadedFileType::GzipTar,
+                downloaded_directory: String::from("air-0.1.0"),
+                downloaded_binary_path: String::from("air-0.1.0/air-aarch64-apple-darwin/air")
+            }
+        );
+
+        assert_eq!(
+            GithubReleaseDetails::new(
+                zed_extension_api::Os::Linux,
+                zed_extension_api::Architecture::X8664,
+                String::from("0.2.0"),
+            ),
+            GithubReleaseDetails {
+                asset_name: String::from("air-x86_64-unknown-linux-gnu.tar.gz"),
+                downloaded_file_type: zed_extension_api::DownloadedFileType::GzipTar,
+                downloaded_directory: String::from("air-0.2.0"),
+                downloaded_binary_path: String::from("air-0.2.0/air-x86_64-unknown-linux-gnu/air")
+            }
+        );
+
+        assert_eq!(
+            GithubReleaseDetails::new(
+                zed_extension_api::Os::Windows,
+                zed_extension_api::Architecture::X86,
+                String::from("0.1.0"),
+            ),
+            GithubReleaseDetails {
+                asset_name: String::from("air-x86-pc-windows-msvc.zip"),
+                downloaded_file_type: zed_extension_api::DownloadedFileType::Zip,
+                downloaded_directory: String::from("air-0.1.0"),
+                downloaded_binary_path: String::from("air-0.1.0/air.exe")
+            }
+        );
+    }
+}
