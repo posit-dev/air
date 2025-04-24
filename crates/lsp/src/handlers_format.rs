4702721fc62c7e8d18a738fe859ada721f51c81f
Davis Vaughan <davis@posit.co>
2025-04-24 11:42:00 -0400

    Revise `Parse` implementation for usage in `Document` (#316)
    * Revise `Parse` implementation for usage in `Document`
    
    * We also use this in the conversion from `Parse` to `AnyParse`
    
    * Generalize formatting tools (#317)

--- crates/lsp/src/handlers_format.rs
+++ crates/lsp/src/handlers_format.rs
@@ -5,11 +5,12 @@
 //
 //
 
-use air_r_formatter::format_node;
 use air_r_syntax::{RExpressionList, RSyntaxKind, RSyntaxNode, WalkEvent};
 use biome_rowan::{AstNode, Language, SyntaxElement};
 use biome_text_size::{TextRange, TextSize};
 use tower_lsp::lsp_types;
+use workspace::format::format_source_with_parse;
+use workspace::format::FormattedSource;
 
 use crate::file_patterns::is_document_excluded_from_formatting;
 use crate::main_loop::LspState;
@@ -43,22 +44,23 @@ pub(crate) fn document_formatting(
         }
     }
 
-    if doc.parse.has_errors() {
+    if doc.parse.has_error() {
         // Refuse to format in the face of parse errors, but only log a warning
         // rather than returning an LSP error, as toast notifications here are distracting.
-        tracing::warn!(
-            "Failed to format {uri}. Can't format when there are parse errors.",
-            uri = params.text_document.uri
-        );
+        tracing::warn!("Failed to format {uri}. Can't format when there are parse errors.");
         return Ok(None);
     }
 
     let format_options = workspace_settings.to_format_options(&doc.contents, &doc.settings);
-    let formatted = format_node(format_options, &doc.parse.syntax())?;
-    let output = formatted.print()?.into_code();
 
-    let edits = to_proto::replace_all_edit(&doc.line_index, &doc.contents, &output)?;
-    Ok(Some(edits))
+    match format_source_with_parse(&doc.contents, &doc.parse, format_options)? {
+        FormattedSource::Changed(formatted) => Ok(Some(to_proto::replace_all_edit(
+            &doc.line_index,
+            &doc.contents,
+            &formatted,
+        )?)),
+        FormattedSource::Unchanged => Ok(None),
+    }
 }
 
 #[tracing::instrument(level = "info", skip_all)]
@@ -88,13 +90,10 @@ pub(crate) fn document_range_formatting(
         }
     }
 
-    if doc.parse.has_errors() {
+    if doc.parse.has_error() {
         // Refuse to format in the face of parse errors, but only log a warning
         // rather than returning an LSP error, as toast notifications here are distracting.
-        tracing::warn!(
-            "Failed to format {uri}. Can't format when there are parse errors.",
-            uri = params.text_document.uri
-        );
+        tracing::warn!("Failed to format {uri}. Can't format when there are parse errors.");
         return Ok(None);
     }
 
