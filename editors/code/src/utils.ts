2dc1027cf64b09875d8d831091885c5ea266e522
Davis Vaughan <davis@posit.co>
2025-07-24 15:11:53 -0400

    Add `fileExists()` utility

--- editors/code/src/utils.ts
+++ editors/code/src/utils.ts
@@ -1,3 +1,4 @@
+import * as vscode from "vscode";
 import * as url from "url";
 import * as path from "path";
 
@@ -10,4 +11,21 @@ export function normalizePath(file: string): string {
 		file = url.fileURLToPath(file);
 	}
 	return path.normalize(file);
+}
+
+/**
+ * Determine whether or not a file exists
+ *
+ * `vscode.workspace.fs.*` does not provide a way to check if a Uri exists or not,
+ * so this is supposedly the next best way to do so.
+ *
+ * This also works on directories.
+ */
+export async function fileExists(uri: vscode.Uri): Promise<boolean> {
+	try {
+		await vscode.workspace.fs.stat(uri);
+		return true;
+	} catch {
+		return false;
+	}
 }
