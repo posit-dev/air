c5ef497e01e9dd46de0a61842c49b39a350bd6d7
Davis Vaughan <davis@posit.co>
2026-05-29 12:10:00 -0400

    Update `air_r_syntax`

--- crates/air_r_syntax/src/string_ext.rs
+++
@@ -1,8 +0,0 @@
-use crate::{RStringValue, inner_string_text};
-use biome_rowan::{SyntaxResult, TokenText};
-
-impl RStringValue {
-    pub fn inner_string_text(&self) -> SyntaxResult<TokenText> {
-        Ok(inner_string_text(&self.value_token()?))
-    }
-}
